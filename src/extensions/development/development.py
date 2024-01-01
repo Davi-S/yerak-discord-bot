9313f01e7d829c4a3ecc7229c14589a4b4e8f1e9
Davi S <67160664+Davi-S@users.noreply.github.com>
2024-01-01 15:48:36 -0300

    Development (#13)
    • Added more development commands
    - Extension related commands

--- src/extensions/development/development.py
+++ src/extensions/development/development.py
@@ -32,29 +32,60 @@ class Development(commands.Cog, command_attrs=dict(hidden=True)):
     @commands.command(**get_command_attributes('reload'))
     async def reload(self, ctx: commands.Context, *extensions: str) -> None:
         to_reload = [ext.split('.')[-1] for ext in self.bot.extensions.keys()] if extensions[0] == 'all' else extensions
-        success = []
-        fail = []
-        for extension in to_reload:
-            try:
-                await self.bot.reload_extension(f'extensions.{extension}')
-                success.append(extension)
-                logger.info(f'Extension "{extension}" reloaded successfully')
-            except Exception as error:
-                fail.append(extension)
-                logger.error(f'Failed to reload the extension "{extension}" due to error: {error}')
-        
-        success_message = f'Extension(s): "{", ".join(success)}" reloaded successfully' if success else ''
-        failure_message = f'Failed to reload the extension(s): "{", ".join(fail)}"' if fail else ''
-        await ctx.reply(success_message + failure_message)
+        action = 'reload'
+        result = await self.manage_extensions(to_reload, action)
+        await ctx.reply(self._format_extensions_message(result, action))
+
+    @commands.command(**get_command_attributes('load'))
+    async def load(self, ctx: commands.Context, *extensions: str) -> None:
+        action = 'load'
+        result = await self.manage_extensions(extensions, action)
+        await ctx.reply(self._format_extensions_message(result, action))
+
+    @commands.command(**get_command_attributes('unload'))
+    async def unload(self, ctx: commands.Context, *extensions: str) -> None:
+        action = 'unload'
+        result = await self.manage_extensions(extensions, action)
+        await ctx.reply(self._format_extensions_message(result, action))
 
     @commands.command(**get_command_attributes('sync'))
     async def sync(self, ctx: commands.Context) -> None:
         synced = await self.bot.tree.sync()
         await ctx.reply(f'Synced {len(synced)} commands successfully')
         logger.info(f'Synced {len(synced)} commands successfully')
-        
+
     @commands.command(**get_command_attributes('close'))
     async def close(self, ctx: commands.Context) -> None:
         await ctx.reply(f'Closing bot')
         logger.info(f'Closing bot')
         await self.bot.close()
+
+    async def manage_extensions(self, extensions: list[str], action: str) -> dict[str, list]:
+        success = []
+        fail = []
+        action_mapping = {
+            'unload': self.bot.unload_extension,
+            'load': self.bot.load_extension,
+            'reload': self.bot.reload_extension,
+        }
+
+        extension_action = action_mapping.get(action)
+        if extension_action is None:
+            logger.error('Failed to manage extensions due to invalid action')
+            raise ValueError('Failed to manage extensions due to invalid action')
+
+        for extension in extensions:
+            try:
+                await extension_action(f'extensions.{extension}')
+                success.append(extension)
+                logger.info(f'Extension "{extension}" {action}ed successfully')
+            except commands.ExtensionError as error:
+                fail.append((extension, error))
+                logger.error(f'Failed to {action} the extension "{extension}" due to error: {error}')
+
+        return {'success': success, 'fail': fail}
+
+    def _format_extensions_message(self, result: dict[str, list], action: str) -> str:
+        success_message = f'Extension(s): "{", ".join(result["success"])}" {action}ed successfully' if result["success"] else ''
+        failure_message = f'Failed to {action} the extension(s): "{", ".join([f"{fail[0]} -> {fail[1]}" for fail in result["fail"]])}"' if result["fail"] else ''
+        return success_message + failure_message
