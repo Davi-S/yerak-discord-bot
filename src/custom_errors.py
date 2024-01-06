784ee638166cc416f81c950fd8d77d03e733a78a
Davi S <davialvessampaio00@gmail.com>
2024-01-06 20:51:41 -0300

    CustomVoiceClient and AudioQueue

--- src/custom_errors.py
+++ src/custom_errors.py
@@ -4,6 +4,10 @@ from discord.ext import commands
 class NotAuthorizedUser(commands.CommandError):
     pass
 
-
+# TODO: make better errors
 class YTDLError(Exception):
+    pass
+
+
+class VoiceError(Exception):
     pass
--- src/extensions/music/music.py
+++ src/extensions/music/music.py
@@ -3,11 +3,13 @@ import collections
 import functools
 import logging
 import random
+import typing as t
 from pathlib import Path
 
 import discord
 # import youtube_dl
 import yt_dlp as youtube_dl
+from async_timeout import timeout
 from discord.ext import commands
 
 import custom_errors as ce
@@ -91,7 +93,100 @@ class AudioSource(discord.PCMVolumeTransformer):
                 times.append(f'{value} {unit}' if value == 1 else f'{value} {unit}s')
 
         return ', '.join(times)
+    
+    
+class AudioQueue(asyncio.Queue[AudioSource]):
+    def _init(self, maxsize) -> None:
+        # TODO: check the maxsize
+        # This is here for type-hints/IDE reasons
+        self._queue: t.Deque[AudioSource] = collections.deque()
 
+    def _put(self, item: AudioSource) -> None:
+        if not isinstance(item, AudioSource):
+            raise TypeError('AudioQueue can only hold AudioSource objects')
+        self._queue.append(item)
+        
+    def clear(self) -> None:
+        self._queue.clear()
+
+    def shuffle(self) -> None:
+        random.shuffle(self._queue)
+
+    def remove(self, index: int) -> None:
+        if 0 <= index < len(self._queue):
+            del self._queue[index]
+        else:
+            raise IndexError("Index out of range")
+
+
+class CustomVoiceClient(discord.VoiceClient):
+    def __init__(self, client, channel,
+            timeout: int = 180,
+            on_play_callback: t.Callable[[t.Any, t.Any], t.Awaitable[t.Any]] = None,
+            on_play_callback_kwargs: dict = None
+    ) -> None:
+        super().__init__(client, channel)
+        self.queue = AudioQueue(20)
+        self.next_event = asyncio.Event()
+        self.looping = False
+        self.audio_player_task = self.client.loop.create_task(self.audio_player())
+        self.timeout = timeout
+        self.current_audio: AudioSource = None
+        
+        if on_play_callback is None:
+            on_play_callback = self._default_on_play_callback
+        if on_play_callback_kwargs is None:
+            on_play_callback_kwargs = {}
+        self.on_play_callback = on_play_callback
+        self.on_play_kwargs = on_play_callback_kwargs
+        
+    @property
+    def volume(self):
+        return self.current_audio.volume
+
+    @volume.setter
+    def volume(self, value: float):
+        self.current_audio.volume = value
+        
+    async def audio_player(self):
+        while True:
+            self.next_event.clear()
+            if not self.looping:
+                # Try to get the next song within 3 minutes.
+                # If no song is added to the queue in time, the player will disconnect due to performance reasons.
+                try:
+                    async with timeout(self.timeout):
+                        self.current_audio = await self.queue.get()
+                except asyncio.TimeoutError:
+                    self.client.loop.create_task(self.disconnect())
+                    return
+
+            # TODO: check and understand this volume part
+            # self.current_audio.volume = self.volume
+            self.play(self.current_audio, after=self.play_next)
+            await self.on_play_callback(**self.on_play_kwargs)
+            await self.next_event.wait()
+    
+    def play_next(self, error=None):
+        if error:
+            raise ce.VoiceError(str(error))
+        self.next_event.set()
+        
+    def skip(self):
+        if self.is_playing():
+            # Stopping the current audio will trigger the play_next method and start the next song if any available on the queue
+            self.stop()
+
+    async def disconnect(self, *, force: bool = False) -> None:
+        self.queue.clear()
+        return await super().disconnect(force=force)
+        
+    async def _default_on_play_callback(self, **kwargs):
+        # TODO add args too (not only kwargs)
+        return
+    
+    def __del__(self):
+        self.audio_player_task.cancel()
 
 class Music(commands.GroupCog):
     """Play songs on a voice channel"""
@@ -103,7 +198,9 @@ class Music(commands.GroupCog):
     async def join(self, ctx: commands.Context):
         destination = ctx.author.voice.channel
         if ctx.guild.voice_client is None:
-            await destination.connect()
+            # Attention to the custom class in the connect function.
+            # This class is now the type of ctx.guild.voice_client
+            await destination.connect(cls=CustomVoiceClient)
         else:
             await ctx.guild.voice_client.move_to(destination)
         await ctx.reply(f'Joined channel {destination.name}')
@@ -122,9 +219,9 @@ class Music(commands.GroupCog):
             await ctx.invoke(self.join)
 
         async with ctx.typing():
-            source = await AudioSource.create_source(ctx, search)
-            ctx.guild.voice_client.play(source)
-            await ctx.reply(f'Playing {search}')
+            audio_source = await AudioSource.create_source(ctx, search)
+            await ctx.guild.voice_client.queue.put(audio_source)
+            await ctx.reply(f'Enqueued {search}')
 
     @join.before_invoke
     @play.before_invoke
@@ -138,7 +235,7 @@ class Music(commands.GroupCog):
             return await ctx.reply('Nothing being played at the moment.')
         if 0 > volume > 100:
             return await ctx.reply('Volume must be between 0 and 100')
-        ctx.guild.voice_client.source.volume = volume / 100
+        ctx.guild.voice_client.volume = volume / 100
         await ctx.reply(f'Volume of the player set to {volume}%')
 
     @commands.hybrid_command(**get_command_attributes('pause'))
