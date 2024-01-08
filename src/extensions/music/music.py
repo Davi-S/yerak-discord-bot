27000eae866fe70937f096906a2f991e1fb3c10c
Davi S <davialvessampaio00@gmail.com>
2024-01-07 23:18:35 -0300

    Using CustomContext
    • Changed ctx.guild.voice_client -> ctx.voice_client
    • Changed commands.Context -> custom_context.CustomContext

--- src/extensions/music/music.py
+++ src/extensions/music/music.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+import custom_context as cc
 
 from discord.ext import commands
 
@@ -17,7 +18,7 @@ FFMPEG_PATH = THIS_FOLDER/'ffmpeg.exe'
 _commands_attributes = exts.read_commands_attributes(THIS_FOLDER/'commands_attr.json')  # Global cache for config data
 get_command_attributes = exts.get_command_attributes_builder(_commands_attributes)
 
-# TODO: there is a logging errors some times
+# TODO: there is a logging error some times
 
 class Music(commands.GroupCog):
     """Play songs on a voice channel"""
@@ -26,55 +27,56 @@ class Music(commands.GroupCog):
         self.bot = bot
 
     @commands.hybrid_command(**get_command_attributes('join'))
-    async def join(self, ctx: commands.Context):
+    async def join(self, ctx: cc.CustomContext):
         destination = ctx.author.voice.channel
-        if ctx.guild.voice_client is None:
+        if ctx.voice_client is None:
             # Attention to the custom class in the connect function.
-            # This class is now the type of ctx.guild.voice_client
+            # This class is now the type of ctx.voice_client
             await destination.connect(cls=CustomVoiceClient)
-        else:
-            await ctx.guild.voice_client.move_to(destination)
+        else: 
+            await ctx.voice_client.move_to(destination)
         await ctx.reply(f'Joined channel {destination.name}')
 
     @commands.hybrid_command(**get_command_attributes('leave'))
-    async def leave(self, ctx: commands.Context):
-        if voice := ctx.guild.voice_client:
+    async def leave(self, ctx: cc.CustomContext):
+        if voice := ctx.voice_client:
             await voice.disconnect()
             await ctx.reply('Disconnected')
             return
         await ctx.reply('Not connected to any voice channel')
 
     @commands.hybrid_command(**get_command_attributes('play'))
-    async def play(self, ctx: commands.Context, *, search: str):
-        if not ctx.guild.voice_client:
+    async def play(self, ctx: cc.CustomContext, *, search: str):
+        if not ctx.voice_client:
             await ctx.invoke(self.join)
 
         async with ctx.typing():
+            # TODO: Remove this AudioSource from here. The CustomVoiceClient should be responsible for handling all
             audio_source = await AudioSource.create_source(ctx, search)
-            await ctx.guild.voice_client.queue.put(audio_source)
+            await ctx.voice_client.queue.put(audio_source)
             await ctx.reply(f'Enqueued {search}')
 
     @commands.hybrid_command(**get_command_attributes('pause'))
-    async def pause(self, ctx: commands.Context):
-        if ctx.guild.voice_client and ctx.guild.voice_client.is_playing():
-            ctx.guild.voice_client.pause()
+    async def pause(self, ctx: cc.CustomContext):
+        if ctx.voice_client and ctx.voice_client.is_playing():
+            ctx.voice_client.pause()
             await ctx.reply('Paused')
 
     @commands.hybrid_command(**get_command_attributes('resume'))
-    async def resume(self, ctx: commands.Context):
-        if ctx.guild.voice_client and not ctx.guild.voice_client.is_playing():
-            ctx.guild.voice_client.resume()
+    async def resume(self, ctx: cc.CustomContext):
+        if ctx.voice_client and not ctx.voice_client.is_playing():
+            ctx.voice_client.resume()
             await ctx.reply('Resumed')
 
     @commands.hybrid_command(**get_command_attributes('stop'))
-    async def stop(self, ctx: commands.Context):
-        if ctx.guild.voice_client.is_playing():
-            ctx.guild.voice_client.stop()
+    async def stop(self, ctx: cc.CustomContext):
+        if ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
             await ctx.reply('Stopped')
 
     @join.before_invoke
     @play.before_invoke
-    async def ensure_voice_state(self, ctx: commands.Context):
+    async def ensure_voice_state(self, ctx: cc.CustomContext):
         if not ctx.author.voice or not ctx.author.voice.channel:
             raise commands.CommandError('You are not connected to any voice channel.')
 
