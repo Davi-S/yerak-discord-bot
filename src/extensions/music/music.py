ec54800cb36085c9179a016ec621011fc2449fae
Davi S <davialvessampaio00@gmail.com>
2024-01-12 16:36:31 -0300

    Replaced "before_invoke" with "checks"

--- src/extensions/music/music.py
+++ src/extensions/music/music.py
@@ -14,7 +14,35 @@ THIS_FOLDER = Path(__file__).parent
 FFMPEG_PATH = THIS_FOLDER/'ffmpeg.exe'
 
 _commands_attributes = exts.read_commands_attributes(THIS_FOLDER/'commands_attr.json')  # Global cache for config data
-get_command_attributes = exts.get_command_attributes_builder( _commands_attributes)
+get_command_attributes = exts.get_command_attributes_builder(_commands_attributes)
+
+
+# CHECKS
+def ensure_author_voice():
+    def predicate(ctx: cc.CustomContext):
+        return ctx.author.voice and ctx.author.voice.channel
+    return commands.check(predicate)
+
+
+def ensure_bot_voice():
+    def predicate(ctx: cc.CustomContext):
+        return ctx.voice_client
+    return commands.check(predicate)
+
+
+def ensure_bot_playing():
+    def predicate(ctx: cc.CustomContext):
+        voice = ctx.voice_client
+        return voice and voice.is_playing()
+    return commands.check(predicate)
+
+
+def ensure_bot_paused():
+    def predicate(ctx: cc.CustomContext):
+        voice = ctx.voice_client
+        return voice and ctx.voice_client.is_paused()
+    return commands.check(predicate)
+
 
 # TODO: there is a logging error some times
 # TODO: COMMANDS
@@ -27,7 +55,7 @@ get_command_attributes = exts.get_command_attributes_builder( _commands_attribut
 # stop
 # skip
 # volume
-# goto 
+# goto
 # lyrics
 # loop
 # nowplaying
@@ -43,6 +71,7 @@ class Music(commands.GroupCog):
         self.bot = bot
 
     @commands.hybrid_command(**get_command_attributes('join'))
+    @ensure_author_voice()
     async def join(self, ctx: cc.CustomContext):
         destination = ctx.author.voice.channel
         if ctx.voice_client is None:
@@ -54,11 +83,13 @@ class Music(commands.GroupCog):
         await ctx.reply(f'Joined channel {destination.name}')
 
     @commands.hybrid_command(**get_command_attributes('leave'))
+    @ensure_bot_voice()
     async def leave(self, ctx: cc.CustomContext):
         await ctx.voice_client.disconnect()
         await ctx.reply('Disconnected')
 
     @commands.hybrid_command(**get_command_attributes('play'))
+    @ensure_bot_voice()
     async def play(self, ctx: cc.CustomContext, *, search: str):
         async with ctx.typing():
             # Put the audio in the queue. If this is the only audio in the queue, it will start automatically
@@ -67,40 +98,17 @@ class Music(commands.GroupCog):
         await ctx.reply(f'Enqueued {search}')
 
     @commands.hybrid_command(**get_command_attributes('pause'))
+    @ensure_bot_playing()
     async def pause(self, ctx: cc.CustomContext):
         ctx.voice_client.pause()
         await ctx.reply('Paused')
 
     @commands.hybrid_command(**get_command_attributes('resume'))
+    @ensure_bot_paused()
     async def resume(self, ctx: cc.CustomContext):
         ctx.voice_client.resume()
         await ctx.reply('Resumed')
-    
+
     @commands.hybrid_command(**get_command_attributes('stop'))
     async def stop(self, ctx: cc.CustomContext):
         pass
-    
-    # TODO: use "checks"
-    # TODO: handle errors raised here
-    @join.before_invoke
-    async def ensure_author_voice(self, ctx: cc.CustomContext):
-        if not ctx.author.voice or not ctx.author.voice.channel:
-            raise commands.CommandError('You are not connected to a voice channel.')
-
-    @play.before_invoke
-    @leave.before_invoke
-    async def ensure_bot_voice(self, ctx: cc.CustomContext):
-        if not ctx.voice_client:
-            raise commands.CommandError('Bot it not connected to a voice channel')
-    
-    @pause.before_invoke
-    async def ensure_bot_playing(self, ctx: cc.CustomContext):
-        voice = ctx.voice_client
-        if (not voice) or (not voice.is_playing()):
-            raise commands.CommandError('Bot it not playing anything')
-    
-    @resume.before_invoke
-    async def ensure_bot_paused(self, ctx: cc.CustomContext):
-        voice = ctx.voice_client
-        if (not voice) or (not ctx.voice_client.is_paused()):
-            raise commands.CommandError('Bot it not paused')
